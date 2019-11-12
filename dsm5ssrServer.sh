40aeb1e8306b345bf7c98bbfa457fd440c8ea29b
sunliang <sunliang711@163.com>
2019-11-12 23:01:32 +0800

    fix bug:missing shebang

--- dsm5ssrServer.sh
+++ dsm5ssrServer.sh
@@ -1,10 +1,3 @@
+#!/bin/sh
 cd /root/DSM5ssrServer/ssr
-case $1 in
-    start)
-        nohup python server.py -c config.server.json > server.log 2>&1 &
-        ;;
-    stop)
-        pid=$(ps | grep 'python server.py'|grep -v grep|awk '{print $1}')
-        kill -9 $pid
-        ;;
-esac
+/usr/bin/python server.py -c config.server.json > ssrserver.log 2>&1 &
